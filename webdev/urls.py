9c0003d7376d850546c3c0da6cb94a7caa3fa8f0
Leandro <aluno103.23187369leandro@gmail.com>
2024-04-04 19:38:03 -0300

    Finalização da pagina

--- webdev/urls.py
+++ webdev/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import re_path, path
 from django.contrib import admin
-from django.urls import path
+from contatos import views
 
 urlpatterns = [
+    
+    path('', views.show),
+    path('add/', views.add),
     path('admin/', admin.site.urls),
 ]
